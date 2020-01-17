d3697548127c14c1bf2a2fbe19015b10fa8e04da
iguana499 <40342696+iguana499@users.noreply.github.com>
2020-01-17 23:02:08 +0400

    lab1

---
+++ FormCar.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PT_lab_1
+{
+    partial class FormCar
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCar));
+            this.pictureBoxCar = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxСars
+            // 
+            this.pictureBoxCar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxCar.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCar.Name = "pictureBoxCars";
+            this.pictureBoxCar.Size = new System.Drawing.Size(884, 461);
+            this.pictureBoxCar.TabIndex = 0;
+            this.pictureBoxCar.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 406);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(128, 43);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "дополнение";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click_1);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(745, 361);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(44, 39);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(688, 380);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(37, 39);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(745, 406);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(42, 39);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(812, 380);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 39);
+            this.buttonRight.TabIndex = 5;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormAutotrain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxCar);
+            this.Name = "FormAutotrain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Автопоезд";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCar;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+    }
+}
---
+++ FormCar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT_lab_1
+{
+    public partial class FormCar : Form
+    {
+        private autotrain autotrain;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public FormCar()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Метод отрисовки машины
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxCar.Width, pictureBoxCar.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            autotrain.DrawCar(gr);
+            pictureBoxCar.Image = bmp;
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "Создать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
+        /// <summary>
+        /// Обработка нажатия кнопок управления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    autotrain.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    autotrain.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    autotrain.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    autotrain.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        private void buttonCreate_Click_1(object sender, EventArgs e)
+        {
+
+            Random rnd = new Random();
+            autotrain = new autotrain(rnd.Next(100, 120), rnd.Next(1000, 2000), Color.Green,
+           Color.Yellow, true, false, true, false);
+            autotrain.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxCar.Width,
+           pictureBoxCar.Height);
+            Draw();
+
+        }
+    }
+}
