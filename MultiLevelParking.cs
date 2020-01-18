98af04ffeb4745ff8d96f05afb94bb12d1ee29d5
iguana499 <40342696+iguana499@users.noreply.github.com>
2020-01-18 12:45:36 +0400

    Исправил

--- MultiLevelParking.cs
+++ MultiLevelParking.cs
@@ -65,11 +65,6 @@ namespace PT_lab_1
             }
             return true;
         }
-        private void WriteToFile(string text, FileStream stream)
-        {
-            byte[] info = new UTF8Encoding(true).GetBytes(text);
-            stream.Write(info, 0, info.Length);
-        }
         public bool LoadData(string filename)
         {
             if (!File.Exists(filename))
